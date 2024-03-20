93a454f0ff156b651415f0c18c07f736e9306a72
RRvW <rrvanwijk@operamail.com>
2024-03-20 11:33:51 +0100

    update to v0.78.1

--- pkgs/rust_hdl/default.nix
+++ pkgs/rust_hdl/default.nix
@@ -10,7 +10,7 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "rust_hdl";
-  version = "0.77.0";
+  version = "0.78.1";
 
   broken = true; # latest version has not been uploaded to crates.io, and source does not contain cargo.lock
   # Furthermore, the vhdl_libraries folder required for operation is not bundled in the crate source
@@ -19,11 +19,11 @@ rustPlatform.buildRustPackage rec {
     owner = "VHDL-LS";
     repo = "rust_hdl";
     rev = "v${version}";
-    sha256 = "0x66rmn0i1p84iivhppp7r0rb1afkwp4hi4nf71fdsl2y6dvh1r0";
+    sha256 = "sha256-LY9lFZe8MFuDwtNbi9D4JiYY+xKb5bGBHGnH951oRiQ=";
   };
 
   doCheck = lib.versionAtLeast rustPlatform.rust.rustc.version "1.70.0"; # running tests requires rust > 1.70
-  cargoSha256 = "sha256-lN9icvwOmXDOkPNbRyV68o18t4fKDO8giHNUSo975F8=";
+  cargoSha256 = "sha256-TdsGymW2nVn1MmrDfBP9pcAhlDFRv2rrl85nQ46IRnM=";
 
   meta = with lib; {
     homepage = "https://github.com/VHDL-LS/rust_hdl";
